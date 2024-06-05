4e20a29c59a0eff99c054173a15699965352e518
Ângelo Carnevale <angelo.carnevale@hotmail.com>
2024-06-05 12:52:00 +0000

    package: install usedapp and ethers

--- next-web-setup/src/providers/DAppProvider.tsx
+++ next-web-setup/src/providers/DAppProvider.tsx
@@ -1,7 +1,16 @@
-import {} from "@usedapp"
+import { Config, Goerli, Mainnet, DAppProvider as Provider } from "@usedapp/core"
+import { getDefaultProvider } from "ethers"
 
-export default function DAppProvider() {
+const config: Config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: getDefaultProvider('mainnet'),
+    [Goerli.chainId]: getDefaultProvider('goerli'),
+  },
+}
+
+export default function DAppProvider({ children }: { children: React.ReactNode }) {
   return (
-    <div>DAppProvider</div>
+    <Provider config={config}>{children}</Provider>
   )
 }
